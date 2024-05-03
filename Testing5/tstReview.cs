5ae19027ee654b48add55b5e947386fd78e92171
P2727240 <P2727240@GWHXX282WIN007W>
2024-05-03 10:54:22 +0100

    Added testing for review

---
+++ Testing5/tstReview.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstReview
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //test to see that it exists
+            Assert.IsNotNull(AnReview);
+        }
+    }
+}

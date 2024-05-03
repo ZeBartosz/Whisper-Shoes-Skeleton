27c129cd8838dbeb2d4be0b991fa825a4a40ea5a
ZeBartosz <stachowiak725@gmail.com>
2024-05-03 10:10:31 +0100

    Added test and class file for stock

---
+++ Testing1/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            clsStock aStock = new clsStock();
+            Assert.IsNotNull(aStock);
+
+        }
+    }
+}

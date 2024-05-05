6bdc4b4bb814e0cc470240daff500a6a4d4962bb
ZeBartosz <stachowiak725@gmail.com>
2024-05-05 23:28:04 +0100

    Added test and class for review

---
+++ Testing5/tstReview.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstReview
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            clsReview aReview = new clsReview();
+            Assert.IsNotNull(aReview);
+
+        }
+    }
+}

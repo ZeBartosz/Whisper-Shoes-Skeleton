bc4b7f641efe2e06a8eb418f8822e8cc4430a63e
lcfcethan <164203156+lcfcethan@users.noreply.github.com>
2024-05-08 15:52:58 +0100

    properties

--- ClassLibrary/clsCustomers.cs
+++ ClassLibrary/clsCustomers.cs
@@ -1,9 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomers
     {
         public clsCustomers()
         {
         }
+
+        public bool Save_Payment_Info { get; set; }
+        public string Customer_First_Name { get; set; }
+        public string Customer_Last_Name { get; set; }
+        public DateTime Customer_DOB { get; set; }
+        public string Customer_Phone_Nmbr { get; set; }
+        public string Customer_Address { get; set; }
     }
 }

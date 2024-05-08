3867b00f0de030e2081b1ed441b8547b6b3ceaed
jor1904 <jamesr2255@gmail.com>
2024-05-08 13:09:33 +0100

    Added testing for staff class

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class
+            clsStaff Staff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(Staff);
+        }
+    }
+}

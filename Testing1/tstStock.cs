b3b3778944eba333d29a4fb64c1c9d229a734883
ZeBartosz <stachowiak725@gmail.com>
2024-05-08 15:00:38 +0100

    Added test for every attribute in tblStock

--- Testing1/tstStock.cs
+++ Testing1/tstStock.cs
@@ -15,5 +15,60 @@ namespace Testing1
             Assert.IsNotNull(aStock);
 
         }
+
+        [TestMethod]
+        public void AutoRestockOK()
+        {
+            clsStock aStock = new clsStock();
+            Boolean TestData = true;
+            aStock.stockAutoRestock = TestData;
+            Assert.AreEqual(aStock.stockAutoRestock, TestData);
+        }
+
+        [TestMethod]
+        public void  LastRestockOk()
+        {
+            clsStock aStock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            aStock.stockLastRestocked = TestData;
+            Assert.AreEqual(aStock.stockLastRestocked, TestData);
+        }
+
+        [TestMethod]
+        public void RestockThresholdOK() 
+        {
+            clsStock aStock = new clsStock();
+            int TestData = 15;
+            aStock.stockRestockThreshold = TestData;
+            Assert.AreEqual(aStock.stockRestockThreshold, TestData);
+        }
+
+        [TestMethod]
+        public void stockQuantityOK() 
+        {
+            clsStock aStock = new clsStock();
+            int TestData = 10;
+            aStock.stockQuantity = TestData;
+            Assert.AreEqual(aStock.stockQuantity, TestData);
+        }
+
+        [TestMethod]
+        public void stockDescriptionOK()
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Whisper Shoes";
+            aStock.stockDescription = TestData;
+            Assert.AreEqual(aStock.stockDescription, TestData); 
+        }
+
+        [TestMethod]
+        public void stockNameOK() 
+        { 
+            clsStock aStock = new clsStock();
+            string TestData = "Adidas";
+            aStock.stockName = TestData;
+            Assert.AreEqual(aStock.stockName, TestData);
+        }
+
     }
 }

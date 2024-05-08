535b2ef297343e55ecc70f9da6c1899f6751226d
P2727240 <P2727240@GWHXX282WIN007W>
2024-05-08 15:55:28 +0100

    Attributes testing

--- Testing5/tstReview.cs
+++ Testing5/tstReview.cs
@@ -15,5 +15,85 @@ namespace Testing5
             Assert.IsNotNull(aReview);
 
         }
+
+        [TestMethod]
+        public void IsApproved()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnReview.IsApproved = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnReview.IsApproved, TestData);
+        }
+
+        [TestMethod]
+        public void DateSubmitted()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnReview.DateSubmitted = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnReview.DateSubmitted, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerName()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //create some test data to assign to the property
+            string TestData = "Akash";
+            //assign the data to the property
+            AnReview.CustomerName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnReview.CustomerName, TestData);
+        }
+
+        [TestMethod]
+        public void ReviewDescription()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //create some test data to assign to the property
+            string TestData = "The packaging of the item is perfect and item also looks good";
+            //assign the data to the property
+            AnReview.ReviewDescription = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnReview.ReviewDescription, TestData);
+        }
+
+        [TestMethod]
+        public void ReviewTitle()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //create some test data to assign to the property
+            string TestData = "About pacakging and item";
+            //assign the data to the property
+            AnReview.ReviewTitle = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnReview.ReviewTitle, TestData);
+        }
+
+        [TestMethod]
+        public void Rating()
+        {
+            //create an instance of the class we want to create
+            clsReview AnReview = new clsReview();
+            //create some test data to assign to the property
+            Int32 TestData = 5;
+            //assign the data to the property
+            AnReview.Rating = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnReview.Rating, TestData);
+        }
     }
 }
+
+

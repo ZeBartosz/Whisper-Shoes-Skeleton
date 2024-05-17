67af98013e5f2c2fb50131a6a99be9d5881b3419
ZeBartosz <stachowiak725@gmail.com>
2024-05-17 11:17:32 +0100

    Added stock collection

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+
+
+        public clsStock ThisStock { get; set; }
+
+        //private variables
+        List<clsStock> mStockList = new List<clsStock>();
+        private Int32 mCount;
+        public List<clsStock> StockList
+        {
+            get 
+            { 
+                return mStockList; 
+            }
+            set 
+            { 
+                mStockList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+       
+    }
+}
---
+++ Testing1/tstStockCollection.cs
@@ -0,0 +1,93 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockCollection allStock = new clsStockCollection(); 
+            Assert.IsNotNull(allStock);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock aStock = new clsStock();
+            aStock.stockId = 1;
+            aStock.stockName = "Test";
+            aStock.stockDescription = "TestTest";
+            aStock.stockQuantity = 10;
+            aStock.stockRestockThreshold = 5;
+            aStock.stockLastRestocked = DateTime.Now;
+            aStock.stockAutoRestock = true;
+
+            TestList.Add(aStock);
+            allStock.StockList = TestList;
+            Assert.AreEqual(allStock.StockList, TestList);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+            Int32 SomeCount = 0;
+            allStock.Count = SomeCount;
+            Assert.AreEqual(allStock.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+            clsStock aStock = new clsStock();
+            aStock.stockId = 1;
+            aStock.stockName = "Test";
+            aStock.stockDescription = "TestTest";
+            aStock.stockQuantity = 10;
+            aStock.stockRestockThreshold = 5;
+            aStock.stockLastRestocked = DateTime.Now;
+            aStock.stockAutoRestock = true;
+
+            allStock.ThisStock = aStock;
+            Assert.AreEqual(allStock.ThisStock, aStock);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock aStock = new clsStock();
+            aStock.stockId = 1;
+            aStock.stockName = "Test";
+            aStock.stockDescription = "TestTest";
+            aStock.stockQuantity = 10;
+            aStock.stockRestockThreshold = 5;
+            aStock.stockLastRestocked = DateTime.Now;
+            aStock.stockAutoRestock = true;
+
+            TestList.Add(aStock);
+            allStock.StockList = TestList;
+            Assert.AreEqual(allStock.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordPresent()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+            Assert.AreEqual(allStock.Count, 2);
+        }
+
+
+    }
+
+  
+}

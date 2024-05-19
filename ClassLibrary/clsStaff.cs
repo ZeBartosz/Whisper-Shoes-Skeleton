c3d5ac7a337b9e3c412c5b1f41928457f36ef4b2
jor1904 <jamesr2255@gmail.com>
2024-05-19 22:28:50 +0100

    Validation tests staff

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -34,6 +34,89 @@ namespace ClassLibrary
             }
           
         }
+
+        public string Valid(string staffName, string staffEmail, string staffPassword, string staffStartDate)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            //create a temporary variable to store the date values
+            DateTime DateTemp;
+
+            //if StaffName is blank
+            if (staffName.Length == 0)
+            {
+                //record the error
+                Error = Error + "You must enter a name :";
+            }
+            //if StaffName is greater than 70 characters
+            if (staffName.Length > 70)
+            {
+                //record the error
+                Error = Error + "Your name must be less than 70 characters : ";
+            }
+
+
+            DateTime DateComp = DateTime.Now.Date;
+            //create an instance of DateTime to compare with DateTemp
+            //in the if statements
+            try
+            {
+                DateTemp = Convert.ToDateTime(staffStartDate);
+                //check to see if the date is less than today's date
+                if (DateTemp < DateComp)
+                {
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                //check to see if date is greater than today's date
+                if (DateTemp > DateComp)
+                {
+                    Error = Error + "The date cannot be on the future : ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error = Error + "The date was not a valid date : ";
+            }
+
+            //if StaffEmail is blank
+            if (staffEmail.Length == 0)
+            {
+                //record the error
+                Error = Error + "You must enter a email :";
+            }
+            //if StaffEmail is greater than 55 characters
+            if (staffEmail.Length > 55)
+            {
+                //record the error
+                Error = Error + "Your email must be less than 55 characters : ";
+            }
+            if (staffEmail.Length < 3)
+            {
+                Error = Error + "Your email must be more than 3 characters :" ;
+            }
+
+            //if StaffPassword is blank
+            if (staffPassword.Length == 0)
+            {
+                //record the error
+                Error = Error + "You must enter a password :";
+            }
+            //if StaffPassword is greater than 55 characters
+            if (staffPassword.Length > 20)
+            {
+                //record the error
+                Error = Error + "Your password must be less than 20 characters : ";
+            }
+            if (staffPassword.Length < 6)
+            {
+                Error = Error + "Your password must be more than 6 characters :";
+            }
+
+
+            return Error;
+
+        }
         //private data member for the staff id property
         private Int32 mStaffId;
 

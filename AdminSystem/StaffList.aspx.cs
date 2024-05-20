449fded9c2070be3646cfa875a89a0fc6949e15b
jor1904 <jamesr2255@gmail.com>
2024-05-20 19:37:12 +0100

    Creating the collection class  - staff

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //display the list box
+            DisplayStaff();
+        }
+
+    }
+
+    void DisplayStaff()
+    {
+        clsStaffCollection Staff = new clsStaffCollection();
+        //set the data source to list the staff in the collection
+        lstStaffList.DataSource = Staff.StaffList;
+        //set the name of the primary key
+        lstStaffList.DataValueField = "StaffId";
+        //set the data feild
+        lstStaffList.DataTextField = "StaffName";
+        //bind the data to the list
+        lstStaffList.DataBind();
 
     }
 }
---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStaffCollection
+    {
+        //private data member for the list
+        List<clsStaff> mStaffList = new List<clsStaff>();
+
+        public clsStaffCollection()
+        {
+            //variable for the index
+            Int32 Index = 0;
+            //variable to store record count
+            Int32 RecordCount = 0;
+            //object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+            //execute stored procedure
+            DB.Execute("sporc_tbStaff_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank staff
+                clsStaff Staff = new clsStaff();
+                //read in the fields for the current record
+                Staff.StaffName = Convert.ToString(DB.DataTable.Rows[Index]["StaffName"]);
+                Staff.StaffEmail = Convert.ToString(DB.DataTable.Rows[Index]["StaffEmail"]);
+                Staff.StaffPassword = Convert.ToString(DB.DataTable.Rows[Index]["StaffPassword"]);
+                Staff.StaffStartDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["StaffStartDate"]);
+                Staff.StaffSalary = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffSalary"]);
+                Staff.StaffManager = Convert.ToBoolean(DB.DataTable.Rows[Index]["StaffManager"]);
+                //add record to private data member
+                mStaffList.Add(Staff);
+                //point at next record
+                Index++;
+
+
+            }
+
+        }
+
+        public List<clsStaff> StaffList
+        {
+            get 
+            {
+                return mStaffList; 
+            }
+            set 
+            { 
+                mStaffList = value; 
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mStaffList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsStaff ThisStaff { get; set; }
+
+       
+
+
+    }
+}

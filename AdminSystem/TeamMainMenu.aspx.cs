40de2208c79e2073870dbc544bd28cb8774da809
P2727240 <P2727240@GWHXX282WIN007W>
2024-06-06 10:37:44 +0100

    Finished

--- AdminSystem/TeamMainMenu.aspx.cs
+++ AdminSystem/TeamMainMenu.aspx.cs
@@ -20,6 +20,7 @@ public partial class TeamMainMenu : System.Web.UI.Page
     protected void btnReviews_Click(object sender, EventArgs e)
     {
         Response.Redirect("ReviewLogin.aspx");
+    }
 
     protected void btnOrder_Click(object sender, EventArgs e)
     {
